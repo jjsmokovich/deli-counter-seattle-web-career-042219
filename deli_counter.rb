eb7095ce228d0ef6de9d3d88467d89e48890489e
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-21 19:37:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,5 +14,5 @@ end
 
 def take_a_number(deli_line, name)
   deli_line << name
-  puts "Welcome, #{name}. You are number #{deli.length} in line."
+  puts "Welcome, #{name}. You are number #{deli_line.length} in line."
 end
