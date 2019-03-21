d4b1f45d89e9710d3caa62a34509f8c096fb015a
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-21 19:30:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,16 @@
-deli_line = ["gib", "tori", "joe"]
+deli_line = ["gib", "tori",deli_line = ["gib", "tori", "joe"]
+
+def line(deli_line)
+  if deli_line.length == 0 
+    puts "The line is currently empty."
+  else 
+    queue = "The line is currently:"
+    deli_line.each.with_index do |cust, line_pos|
+      queue << " #{line_pos+1}. #{cust}"
+    end
+   puts queue
+  end
+end "joe"]
 
 def line(deli_line)
   if deli_line.length == 0 
