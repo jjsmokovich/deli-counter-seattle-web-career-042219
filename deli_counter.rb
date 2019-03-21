98c3b2fb541d2ec0483cdfc315a4bbef140370a9
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-21 19:08:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,4 @@
-deli_line = ["gib", "tori"]
+deli_line = ["gib", "tori", "joe"]
 
 def line(deli_line)
   if deli_line.length == 0 
@@ -6,7 +6,7 @@ def line(deli_line)
   else 
     queue = "The line is currently: "
     deli_line.each.with_index do |cust, line_pos|
-      queue << "#{line_pos+1}. #{cust}"
+      queue << " #{line_pos+1}. #{cust}"
     end
    puts queue
   end
