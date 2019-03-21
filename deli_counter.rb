de778995f4595ba8c958ecc3fff36b298ee7e03a
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-21 19:33:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,18 +10,6 @@ def line(deli_line)
     end
    puts queue
   end
-end 
-
-def line(deli_line)
-  if deli_line.length == 0 
-    puts "The line is currently empty."
-  else 
-    queue = "The line is currently:"
-    deli_line.each.with_index do |cust, line_pos|
-      queue << " #{line_pos+1}. #{cust}"
-    end
-   puts queue
-  end
 end
 
 def take_a_number(deli_line, name)
