d7f71a8a2dd9b7aac28979784a221841dc3a0dc6
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-21 19:01:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(deli_line)
   if deli_line.length == 0 
     puts "The line is currently empty."
   else 
-    queue = []
+    queue = "The line is currently "
     deli_line.each.with_index do |cust, line_pos|
-      queue.push(line_pos+1, cust) 
+      queue.line_pos+1, cust 
     end
    queue
   end
