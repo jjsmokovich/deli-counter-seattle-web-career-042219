fcd3f5e30793065c4169fb70cc7aba98be36f2a5
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-21 18:44:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,8 @@ deli_line = []
 def line(deli_line)
   if deli_line.length == 0 
     puts "The line is currently empty."
+  else 
+    current_line = "The line is currently: "
+    deli_line.each.with_index do |cust| 
   end
 end
